fb6470c8371edd6d5fa01d24c201a05b77069455
flood <flood@DESKTOP-C5V4T47>
2024-03-15 12:41:01 +0100

    Hola

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -1,5 +1,14 @@
 package paquete1;
 
 public class Principal {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Estoy haciendo control de versiones con Git");
+		
+		System.out.println("Otro mensaje");
+		
+	}
 
 }
+
